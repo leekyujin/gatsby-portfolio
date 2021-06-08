c3c4ceb87b9fcf1bf70a0c94135deb947cc0eb9b
zerosum <zhen_87@naver.com>
2021-06-08 18:04:45 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,9 @@ module.exports = {
       resolve: `gatsby-source-datocms`,
       options: {
         apiToken: process.env.DATO_API_TOKEN,
+         schemas: {
+      about: require('./src/schemas/about.json')
+    }
       },
     },
   ],
